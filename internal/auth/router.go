24845090e17c88a4907bdb268089e4c6bc7e6f51
Мишенька <misenka@MacBook-Air-Misenka.local>
2026-08-04 15:48:31 +0300

    update router auth

--- internal/auth/router.go
+++ internal/auth/router.go
@@ -3,12 +3,7 @@ package auth
 import "github.com/gofiber/fiber/v3"
 
 func RegisterRoutes(router fiber.Router, h *Handler) {
-	// group := router.Group("/auth")
-
-	// group.Post("/register", h.Register)
-	// group.Post("/login", h.Login)
-	// group.Post("/refresh", h.Refresh)
-	// group.Post("/logout", h.Logout)
-	// group.Get("/me", Middleware(jwtSecret), h.Me)
-	// group.Patch("/password", Middleware(jwtSecret), h.ChangePassword)
+	group := router.Group("/auth")
+	group.Post("/login", h.Login)
+	group.Post("/validate-session", h.ValidateSession)
 }
