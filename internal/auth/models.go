ed9daffce31664d5990b91ebc19533747afbc13e
Мишенька <misenka@MacBook-Air-Misenka.local>
2026-08-06 12:48:06 +0300

    remove min validate

--- internal/auth/models.go
+++ internal/auth/models.go
@@ -3,8 +3,8 @@ package auth
 import "time"
 
 type LoginRequest struct {
-	Login    string `json:"login" validate:"required,min=3,max=100" example:"admin" description:"User email"`
-	Password string `json:"password" validate:"required,min=8,max=255" example:"P@ssw0rd123" description:"User password"`
+	Login    string `json:"login" validate:"required,max=100" example:"admin" description:"User email"`
+	Password string `json:"password" validate:"required,max=255" example:"P@ssw0rd123" description:"User password"`
 }
 
 type LoginResult struct {
